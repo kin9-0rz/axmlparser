441b33beb741028fd775a715fb50515e42467a07
LAI <acgmohu@gmail.com>
2016-08-11 16:48:12 +0800

    update

---
+++ setup.py
@@ -0,0 +1,24 @@
+﻿import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="axmlparser",
+    version="0.0.2",
+    author="acgmohu",
+    author_email="acgmohu@gmail.com",
+    description=("A parser for AndroidManifest.xml"),
+    license="Apache",
+    keywords="axml manifest",
+    url="https://github.com/acgmohu/axmlparser",
+    py_modules=["axmlparser"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities",
+    ],
+)
